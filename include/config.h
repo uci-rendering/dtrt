c777413b4ecd3eb66422ffcf1fcbd3d5fd637764
holmes969 <zhangchengee@gmail.com>
2020-02-17 23:59:47 -0800

    updated install file

--- include/config.h
+++ include/config.h
@@ -2,7 +2,7 @@
 #ifndef CONFIG_H__
 #define CONFIG_H__
 
-constexpr int nder = 1;
+constexpr int nder = 4;
 constexpr double ShadowEpsilon = 1e-3f;
 constexpr double AngleEpsilon = 1e-4f;
 
--- install.sh
+++ install.sh
@@ -2,16 +2,21 @@
 
 sudo apt-get update && sudo apt-get -y upgrade
 
-sudo apt-get install -y alien dpkg-dev debhelper build-essential libtbb-dev libglfw3-dev libpython-dev libzip-dev python3-pip libopenexr-dev pkg-config libeigen3-dev
+sudo apt-get install -y alien dpkg-dev debhelper build-essential libtbb-dev libglfw3-dev libpython-dev libzip-dev python3-pip libopenexr-dev pkg-config libeigen3-dev libssl-dev
 
 pip3 install torch==1.3.1+cpu torchvision==0.4.2+cpu -f https://download.pytorch.org/whl/torch_stable.html
-pip3 install cmake OpenEXR scikit-image pytest
-
-sudo snap install --classic cmake
-/usr/bin/python3.6 -m pip install pytest
+pip3 install OpenEXR scikit-image pytest
 
 mkdir -p dependencies
 cd dependencies
+
+# Install Cmake (3.16.3)
+wget https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3.tar.gz
+tar -zxvf cmake-3.16.3.tar.gz
+cd cmake-3.16.3
+./bootstrap
+sudo make install -j
+cd ..
 
 git clone https://github.com/embree/embree.git
 git clone https://github.com/pybind/pybind11.git
@@ -21,15 +26,15 @@ cd embree
 mkdir -p build
 cd build
 cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DEMBREE_ISPC_SUPPORT=OFF ..
-sudo make install -j
+sudo make install -j8
 
 # Compile Pybind11
 cd ../../pybind11
 mkdir -p build
 cd build
 cmake ..
-make check -j
-sudo make install
+make check -j8
+sudo make install -j8
 
 echo '''
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
